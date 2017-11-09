edd9f447920144564122682c03fb2ee46b0f79cb
Nikica Trajkovski <trajkovs@usc.edu>
2017-11-08 23:16:35 -0800

    training done

--- project3/src/Naive_Bayes_Classifier.h
+++ project3/src/Naive_Bayes_Classifier.h
@@ -1,15 +1,30 @@
 #ifndef NAIVE_BAYES_CLASSIFIER
 #define NAIVE_BAYES_CLASSIFIER
 
+#include "mnist_reader.hpp"
+#include "mnist_utils.hpp"
+#define MNIST_DATA_DIR "../mnist_data"
 #include <vector>
 
 class Naive_Bayes_Classifier {
 public:
+  Naive_Bayes_Classifier();
   void train();
-  std::vector<int> calculate_priori_probabilities(const std::vector<unsigned char> & labels);
-  std::vector<std::vector<double>> calculate_conditional_probabilities(const std::vector<unsigned char> & labels, const std::vector<std::vector<unsigned char>> & images);
+  void calculate_priori_probabilities(const std::vector<unsigned char> & labels);
+  void calculate_conditional_probabilities(const std::vector<unsigned char> & labels, const std::vector<std::vector<unsigned char>> & images);
   std::vector<std::vector<int>> get_digit_list(const std::vector<unsigned char> & labels);
+
+  void evaluate();
 private:
+  const unsigned int num_training_images = 60000;
+  const unsigned int num_features = 784;
+  const unsigned int num_classes = 10;
+
+  /* Dataset */
+  mnist::MNIST_dataset<std::vector, std::vector<uint8_t>, uint8_t> dataset;
+
+  std::vector<double> priori_probabilities;
+  std::vector<std::vector<double>> probabilities;
 };
 
 #endif
